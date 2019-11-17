ce0c56735a9cba6bcf829f8aa7d4d779d70803cf
AlexStephanov <48553755+AlexStephanov@users.noreply.github.com>
2019-11-17 11:51:35 +0300

    RecyclerView added and connected

--- app/src/main/java/com/alexstephanov/brestnews/MainActivity.kt
+++ app/src/main/java/com/alexstephanov/brestnews/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.alexstephanov.brestnews
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initList()
+    }
+
+    private fun initList() {
+        val list : RecyclerView = findViewById(R.id.recycler_view_main_list)
+
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = DataAdapter(initListItemModels())
+    }
+
+    @SuppressLint("ResourceType")
+    fun initListItemModels(): MutableList<ListItem> {
+
+        val items: MutableList<ListItem> =  mutableListOf()
+        val item = ListItem(R.mipmap.ic_launcher, "Brest", "News")
+
+        repeat(10) {
+            items.add(item)
+        }
+
+        return items
     }
 }
